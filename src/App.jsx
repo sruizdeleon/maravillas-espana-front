5154b5ba3239e82f103e20d1c977c84d3064fa39
sruizdeleon <ruizdeleonsergio8@gmail.com>
2024-04-14 15:02:49 +0200

    Creando ruta a Área privada Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { Routes, Route, Link, Navigate } from 'react-router-dom';
 import './App.css'
+import Home from './pages/home/Home';
 
 function App() {
 
   return (
     <>
-
+      <main>
+        <Routes>
+          <Route path='/home' element={<Home></Home>}></Route>
+        </Routes>
+      </main>
     </>
   )
 }
