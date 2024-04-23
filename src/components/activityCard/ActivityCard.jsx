38984f505f5a94126becdbc4c36ac8fe9cb5ffa5
sruizdeleon <ruizdeleonsergio8@gmail.com>
2024-04-23 22:08:53 +0200

    Refactorizando la Home, sacando un nuevo componente Searcher

--- src/components/activityCard/ActivityCard.jsx
+++ src/components/activityCard/ActivityCard.jsx
@@ -1,18 +1,9 @@
 import { useEffect, useState } from "react";
 import { Link } from 'react-router-dom'
 import "./ActivityCard.css";
+import Paragraph from "../shared/Paragraph";
 
 const ActivityCard = ({ actividad }) => {
-
-	const [titulo, setTitulo] = useState("")
-	const [descripcion, setDescripcion] = useState("")
-
-	useEffect(()=>{
-		if(actividad) {
-			setTitulo(recortarTexto(actividad.nombre, 58))
-			setDescripcion(recortarTexto(actividad.descripcion, 120));
-		}
-	},[])
 
 	function recortarTexto(texto, longitud) {
 		if (texto.length > longitud) {
@@ -26,7 +17,11 @@ const ActivityCard = ({ actividad }) => {
 		<article className="activity-card">
 			<div className="activity-card__header" style={{ backgroundImage: `url(${actividad?.img})` }}>
 				<div className="activity-card__aside">
-					<p className="activity-card__tag">Plan de {actividad.tipo}</p>
+					{actividad.tipo === "ciudad" ? (
+						<p className="activity-card__tag activity-card__tag-city">Plan de {actividad.tipo}</p>
+					) : (
+						<p className="activity-card__tag activity-card__tag-country">Plan {actividad.tipo}</p>
+					)}
 					<svg
 						className="activity-card__icon"
 						xmlns="http://www.w3.org/2000/svg"
@@ -39,11 +34,10 @@ const ActivityCard = ({ actividad }) => {
 						<path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15" />
 					</svg>
 				</div>
-				{/* <img className="activity-card__image" src={actividad?.img} /> */}
 			</div>
 			<div className="activity-card__body">
-				<h3 className="activity-card__title">{titulo}</h3>
-				<p className="activity-card__description">{descripcion}</p>
+				<Paragraph className="activity-card__title" text={actividad.nombre} maxLength={56}></Paragraph>
+				<Paragraph className="activity-card__description" text={actividad.descripcion} maxLength={120}></Paragraph>
 			</div>
 			<div className="activity-card__footer">
 				<Link to={actividad._id}>
@@ -53,6 +47,10 @@ const ActivityCard = ({ actividad }) => {
 					<img className="activity-card__img-province" src={actividad?.provincia.imgenBandera}></img>
 					<p className="activity-card__province">{actividad?.provincia.nombre}</p>
 				</div>
+			</div>
+			<div className="activity-card__admin">
+				<button className="activity-card__admin-button activity-card__admin-button--edit">✏️</button>
+				<button className="activity-card__admin-button activity-card__admin-button--delete">🗑️</button>
 			</div>
 		</article>
 	);
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,6 +5,7 @@ import provinciasData from '../../models/Provincias.json';
 // import { SessionContext } from "../../contexts/SessionContext";
 import axios from 'axios';
 import ActivityCard from '../../components/activityCard/ActivityCard';
+import Searcher from '../../components/searcher/Searcher';
 
 const Home = () => {
 	const DEFAULTDATOS = { comunidad: "", provincia: "", plan: ""}
@@ -12,38 +13,7 @@ const Home = () => {
 	const PROVINCIAS = provinciasData.provincias;
 
 	// const { user } = useContext(SessionContext);
-	const [actividades, setActividades] = useState([
-		// {
-		// 	_id: "662380ef38b0c0dd919cadcd",
-		// 	nombre: "Catedral de León",
-		// 	img: "https://i.postimg.cc/2831scmQ/Pueblos-de-los-Ancares.jpg",
-		// 	descripcion:
-		// 		"Contempla la impresionante arquitectura gótica de la Catedral de León, una de las más destacadas de España.",
-		// 	provincia: {
-		// 		_id: "661c09a6ab5c0cb25b8e9d7f",
-		// 		nombre: "León",
-		// 		imagenBandera: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRu1ADKF86JkG7FKvBIbOPt5OvT4TGONcdjQQ&s",
-		// 	},
-		// 	comunidad: "Castilla y Leon",
-		// 	tipo: "ciudad",
-		// 	__v: 0,
-		// },
-		// {
-		// 	_id: "662380ef38b0c0dd919cadcd",
-		// 	nombre: "Catedral de León",
-		// 	img: "https://i.postimg.cc/2831scmQ/Pueblos-de-los-Ancares.jpg",
-		// 	descripcion:
-		// 		"Contempla la impresionante arquitectura gótica de la Catedral de León, una de las más destacadas de España.",
-		// 	provincia: {
-		// 		_id: "661c09a6ab5c0cb25b8e9d7f",
-		// 		nombre: "León",
-		// 		imagenBandera: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRu1ADKF86JkG7FKvBIbOPt5OvT4TGONcdjQQ&s",
-		// 	},
-		// 	comunidad: "Castilla y Leon",
-		// 	tipo: "ciudad",
-		// 	__v: 0,
-		// },
-	]);
+	const [actividades, setActividades] = useState([]);
 	const [datos, setDatos] = useState(DEFAULTDATOS)
 	const [comunidades, setComunidades] = useState(COMUNIDADES);
 	const [busqueda, setBusqueda] = useState(false);
@@ -178,136 +148,37 @@ const Home = () => {
 
 	return (
 		<>
-			<section className="searcher" style={ busqueda ? { height:  "fit-content"} : {height: "calc(100vh - 154px)", transitionProperty: "height", transitionDuration: "1s", transitionTimingFunction: "linear"}}>
-				<form className="searcher__form">
-					{/* INPUT COMUNIDADES */}
-					<div className="searcher__container searcher__container--border">
-						<div className="searcher__icon-container">
-							<svg
-								className="searcher__icon"
-								xmlns="http://www.w3.org/2000/svg"
-								fill="#666666"
-								class="bi bi-geo-alt-fill"
-								viewBox="0 0 16 16"
-							>
-								<path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6" />
-							</svg>
-						</div>
-						<div className="searcher__input-container">
-							<label className="searcher__label" htmlFor="comunidad">
-								Comunidad
-							</label>
-							<input
-								className="searcher__input"
-								id="comunidad"
-								name="comunidad"
-								multiple
-								list="lista-comunidades"
-								placeholder="Comunidad"
-								value={datos.comunidad}
-								onChange={e => onCambioEnComunidad(e)}
-								disabled={comunidadInputDesactivado}
-							></input>
-							<datalist id="lista-comunidades">
-								{comunidades.map((comunidad, index) => (
-									<option key={index}>{comunidad.name}</option>
-								))}
-							</datalist>
-						</div>
-					</div>
-
-					{/* INPUT PROVINCIAS */}
-					<div className="searcher__container searcher__container--border">
-						<div className="searcher__icon-container">
-							<svg
-								className="searcher__icon"
-								xmlns="http://www.w3.org/2000/svg"
-								fill="#666666"
-								class="bi bi-geo-alt-fill"
-								viewBox="0 0 16 16"
-							>
-								<path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6" />
-							</svg>
-						</div>
-						<div className="searcher__input-container">
-							<label className="searcher__label" htmlFor="provincia">
-								Provincia
-							</label>
-							<input
-								className="searcher__input"
-								id="provincia"
-								name="provincia"
-								multiple
-								list="lista-provincias"
-								placeholder="Provincias"
-								onChange={e => onCambioEnProvincia(e)}
-								disabled={provinciaInputDesactivado}
-							></input>
-							<datalist id="lista-provincias">
-								{provincias.map((provincia, index) => (
-									<option key={index}>{provincia.name}</option>
-								))}
-							</datalist>
-						</div>
-					</div>
-
-					{/* INPUT TIPO DE PLAN */}
-					<div className="searcher__container">
-						<div className="searcher__icon-container">
-							<svg
-								className="searcher__icon"
-								xmlns="http://www.w3.org/2000/svg"
-								fill="#666666"
-								class="bi bi-lightning-charge-fill"
-								viewBox="0 0 16 16"
-							>
-								<path d="M11.251.068a.5.5 0 0 1 .227.58L9.677 6.5H13a.5.5 0 0 1 .364.843l-8 8.5a.5.5 0 0 1-.842-.49L6.323 9.5H3a.5.5 0 0 1-.364-.843l8-8.5a.5.5 0 0 1 .615-.09z" />
-							</svg>
-						</div>
-						<div className="searcher__input-container">
-							<label className="searcher__label" htmlFor="plan">
-								Tipo de plan
-							</label>
-							<input
-								className="searcher__input"
-								id="plan"
-								name="plan"
-								multiple
-								list="lista-planes"
-								placeholder="Tipo de plan"
-								onChange={e => onCambioEnPlan(e)}
-							></input>
-							<datalist id="lista-planes">
-								<option>Plan de ciudad</option>
-								<option>Plan rural</option>
-							</datalist>
-						</div>
-					</div>
-
-					{/* BUTTON BUSCAR */}
-					<button className="searcher__button" type="button" onClick={buscarActividades}>
-						<div className="searcher__button-icon">
-							<svg xmlns="http://www.w3.org/2000/svg" fill="#FFFFFF" class="bi bi-search" viewBox="0 0 16 16">
-								<path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
-							</svg>
-						</div>
-						Buscar
-					</button>
-				</form>
-			</section>
+			<Searcher
+				busqueda={busqueda}
+				datos={datos}
+				provincias={provincias}
+				comunidades={comunidades}
+				onCambioEnComunidad={onCambioEnComunidad}
+				onCambioEnProvincia={onCambioEnProvincia}
+				onCambioEnPlan={onCambioEnPlan}
+				comunidadInputDesactivado={comunidadInputDesactivado}
+				provinciaInputDesactivado={provinciaInputDesactivado}
+				buscarActividades={buscarActividades}
+			></Searcher>
 
 			{/* RESULTADOS DE BÚSQUEDA */}
 			<section className="activities-list">
 				<h2 className="activities-list__title">
-					{actividades.length > 0
-						? datos.provincia !== ""
-							? `Lo más buscado de ${datos.provincia}, ${datos.comunidad}`
-							: `Lo más buscado de ${datos.comunidad}`
-						: ""}
-					<img
-						className="activities-list__flag"
-						src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRu1ADKF86JkG7FKvBIbOPt5OvT4TGONcdjQQ&s"
-					></img>
+					{actividades.length > 0 ? (
+						datos.provincia !== "" ? (
+							<>
+								<h2 className="activities-list__title">
+									Lo más buscado de {datos.provincia}, {datos.comunidad}
+								</h2>
+							</>
+						) : (
+							<>
+								<h2 className="activities-list__title">Lo más buscado de {datos.comunidad}</h2>
+							</>
+						)
+					) : (
+						""
+					)}
 				</h2>
 
 				{/* Bucle con Componente tarjeta actividad */}
