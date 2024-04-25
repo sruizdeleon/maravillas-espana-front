28c6b1be73e7afa22b9ae969c1232f161ebc432b
sruizdeleon <ruizdeleonsergio8@gmail.com>
2024-04-25 17:35:17 +0200

    Creando el formulario de actividad

---
+++ src/components/forms/activityForm/ActivityForm.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const ActivityForm = () => {
+  return (
+    <form>
+      <fieldset>
+        <label htmlFor='nombre'>Nombre de la actividad</label>
+        <input type='text' name='nombre'></input>
+      </fieldset>
+      <fieldset>
+        <label htmlFor='descripcion'>Descripción de la actividad</label>
+        <input type='text' name='descripcion'></input>
+      </fieldset>
+      <fieldset>
+        <textarea></textarea>
+        <label htmlFor='tipo'>Tipo de actividad</label>
+        <input type='text' name='tipo'></input>
+      </fieldset>
+      <fieldset>
+        <label htmlFor='nombre'>Nombre de la actividad</label>
+        <input type='text' name='nombre'></input>
+      </fieldset>
+      <fieldset>
+        <label htmlFor='nombre'>Nombre de la actividad</label>
+        <input type='text' name='nombre'></input>
+      </fieldset>
+      comunidad
+      descripcion
+      img
+      nombre
+      provincia
+      tipo
+    </form>
+  )
+}
+
+export default ActivityForm
