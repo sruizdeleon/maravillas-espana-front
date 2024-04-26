731364d8c1a2a94cee557f35b71b51d12a4fc661
sruizdeleon <ruizdeleonsergio8@gmail.com>
2024-04-26 23:05:18 +0200

    Implementada función de borrar actividad

--- src/components/activityCard/ActivityCard.jsx
+++ src/components/activityCard/ActivityCard.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import "./ActivityCard.css";
 import Paragraph from "../shared/Paragraph";
 
-const ActivityCard = ({ actividad }) => {
+const ActivityCard = ({ actividad, onBorrarActividad }) => {
 
 	function recortarTexto(texto, longitud) {
 		if (texto.length > longitud) {
@@ -44,13 +44,13 @@ const ActivityCard = ({ actividad }) => {
 					<button className="activity-card__button-see-more">Ver más</button>
 				</Link>
 				<div className="activity-card__container-province">
-					<img className="activity-card__img-province" src={actividad?.provincia.imgenBandera}></img>
-					<p className="activity-card__province">{actividad?.provincia.nombre}</p>
+					<img className="activity-card__img-province" src={actividad?.provincia?.imgenBandera}></img>
+					<p className="activity-card__province">{actividad?.provincia?.nombre}</p>
 				</div>
 			</div>
 			<div className="activity-card__admin">
 				<button className="activity-card__admin-button activity-card__admin-button--edit">✏️</button>
-				<button className="activity-card__admin-button activity-card__admin-button--delete">🗑️</button>
+				<button className="activity-card__admin-button activity-card__admin-button--delete" onClick={()=>onBorrarActividad(actividad)}>🗑️</button>
 			</div>
 		</article>
 	);
